59e8508109edfeda73192bd954b559738a707bc2
chowanza <luiscandialesfajardo@gmail.com>
2025-10-10 14:55:19 -0400

    Progreso 10-10-2025 deploy test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuración simplificada
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'http://172.16.0.11:3000' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        ],
+      },
+    ]
+  },
 }
 
-export default nextConfig
+module.exports = nextConfig
